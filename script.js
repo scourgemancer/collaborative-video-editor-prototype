4bbc4903ea5acce5573c0d7bd2967c7bd93d847a
Timothy Geary <scourgemancer@users.noreply.github.com>
2018-10-31 04:10:39 -0400

    Adds IDs and functions for the bars

--- script.js
+++ script.js
@@ -26,34 +26,67 @@ function initializeTabs() {
   initializeShareTab();
   initializeChatTab();
   initializeVideoContainer();
+  initializeBars();
 }
 
 /*Creates the video elements in the videos tab*/
 function initializeVideosTab() {
+  let tab = document.getElementById("Videos");
   //todo - delete background image in css and implement actual content
 }
 
 /*Creates the categrorie and tool elements*/
 function initializeToolsTab() {
+  let tab = document.getElementById("Tools");
   //todo - delete background image in css and implement actual content
 }
 
 /*Creates the social media elements*/
 function initializeShareTab() {
+  let tab = document.getElementById("Share");
   //todo - delete background image in css and implement actual content
 }
 
 /*Creates the chat container and it's initial messages*/
 function initializeChatTab() {
+  let tab = document.getElementById("Chat");
   //todo - delete background image in css and implement actual content
 }
 
 /*Creates the initial video container*/
 function initializeVideoContainer() {
+  let container = document.getElementById("video-container");
   //todo - delete background image in css and implement actual content
 }
 
 /*Opens a local video in the video container*/
 function openVideo(filename) {
+  let container = document.getElementById("video-container");
   //todo - delete background image in css and implement actual content
+  openBars(); // change the bars alongside the video
+}
+
+/*Changes the bars underneath the video to match it*/
+function openBars() {
+  openTopBar();
+  openBottomBar();
+  openScrollbar();
+}
+
+/*Changes the top bar underneath the video to match it*/
+function openTopBar() {
+  let tab = document.getElementById("top-bar");
+  //todo - delete css and implement actual content
+}
+
+/*Changes the bottom bar underneath the video to match it*/
+function openBottomBar() {
+  let tab = document.getElementById("bottom-bar");
+  //todo - delete css and implement actual content
+}
+
+/*Changes the scrollbar for the bars underneath the video*/
+function openBottomBar() {
+  let tab = document.getElementById("scrollbar");
+  //todo - delete css and implement actual content
 }

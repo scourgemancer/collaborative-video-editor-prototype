4b6cdc6db88efc697e273d5fd63f499f2f655e45
Timothy Geary <scourgemancer@users.noreply.github.com>
2018-10-31 02:04:52 -0400

    Initial push with project work so far

---
+++ script.js
@@ -0,0 +1,20 @@
+/*Changes the tab currently being viewed*/
+function openTab(evt, tabName) {
+  //Makes all tab content invisible
+  let tabcontents = document.getElementsByClassName("tabcontent");
+  for (let tabcontent of tabcontents) {
+    tabcontent.style.display = "none";
+  }
+
+  //Marks all tab buttons as inactive
+  tablinks = document.getElementsByClassName("tablinks");
+  for (let tablink of tablinks) {
+    tablink.className = tablink.className.replace(" active", "");
+  }
+
+  //Makes the selected tab visible
+  document.getElementById(tabName).style.display = "block";
+  
+  //Marks the selected tab's button as active
+  evt.currentTarget.className += " active";
+}

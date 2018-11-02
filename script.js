218a7c62bedc482c62de45dd2e0ff6fd0b296849
Sean Bashaw <seanbshw@gmail.com>
2018-11-02 03:26:40 -0400

    Added videos tab
    #tabular

--- script.js
+++ script.js
@@ -18,7 +18,31 @@ function openTab(evt, tabName) {
   //Makes the selected tab visible
   document.getElementById(tabName).style.display = "block";
 }
-
+function addVideo(){
+  var videoDiv = document.getElementById("Videos");
+  var newVid =document.createElement("div");
+  newVid.className = "videoDIV";
+  var vidName = document.createElement("p");
+  vidName.innerHTML="new video";
+  vidName.className = "name";
+  var vidLength = document.createElement("p");
+  vidLength.innerHTML="3:11";
+  vidLength.className = "length";
+    var vidDate = document.createElement("p");
+  vidDate.innerHTML="June 15th, 2018";
+  vidDate.className = "date";
+    var vidSize = document.createElement("p");
+  vidSize.innerHTML="3 MB";
+  vidSize.className = "size";
+var vidImage = document.createElement("IMG");
+vidImage.src="https://image.shutterstock.com/image-photo/funny-man-watermelon-helmet-googles-450w-157354478.jpg";
+newVid.appendChild(vidImage);
+newVid.appendChild(vidName);
+newVid.appendChild(vidLength);
+newVid.appendChild(vidDate);
+newVid.appendChild(vidSize);
+videoDiv.appendChild(newVid);
+}
 /*Create all of the starting content for the tabs*/
 function initializeTabs() {
   initializeVideosTab();
